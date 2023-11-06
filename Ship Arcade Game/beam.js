c81a12c757b13f1c07c25de08f8f3a86327b4973
Bell <Eli.Bell@evergreen.edu>
2023-11-06 11:34:06 -0800

    Fix for player 2 shooting

--- Ship Arcade Game/beam.js
+++ Ship Arcade Game/beam.js
@@ -2,7 +2,6 @@ class Beam extends Phaser.GameObjects.Sprite {
     constructor(scene) {
         var x = scene.player.x;
         var y = scene.player.y;
-        
         super(scene, x, y, "beam");
         scene.add.existing(this);
 
